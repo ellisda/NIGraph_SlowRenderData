884ae6d056dec385b133c247ed16b6388c6a2a77
David Ellis <ellisda@gmail.com>
2013-12-30 11:40:10 -0700

    First commit of a test app that I posted to National Instruments Measurement Studio Forums ref: http://forums.ni.com/t5/Measurement-Studio-for-NET/Why-does-WPF-Vector-Plot-take-so-long-10-sec-to-Render-4x1000/td-p/2671311
    Data is slow to render when zoomed in tight on yAxis (~10 sec maximized on 1600x900)

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NIGraph_SlowRenderData
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            graph1.DataSource = NoisyData.GetMultiPlotData();
+            verticalAxis1.Range = new NationalInstruments.Controls.Range<double>(50, 100); //example range where noisy data goes off the top and bottom of screen
+        }
+    }
+}
